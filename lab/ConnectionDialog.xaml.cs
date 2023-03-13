f9feb05f5f8e83851bf21a2aa62417f9b1449869
XmasApple <y4blokof@yandex.ru>
2023-03-13 12:52:31 +0300

    Part 1: connection

---
+++ lab/ConnectionDialog.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+// mysql connector
+using System.Data.SqlClient;
+using System.Windows.Controls;
+
+namespace lab;
+
+public partial class ConnectionDialog : Window
+{
+    public ConnectionDialog()
+    {
+        InitializeComponent();
+    }
+
+    private void ConnectButton_Click(object sender, RoutedEventArgs e)
+    {
+        var databaseAddress = (TextBox)FindName("DatabaseAddressTextBox")!;
+        var databaseName = (TextBox)FindName("DatabaseNameTextBox")!;
+        var username = (TextBox)FindName("UserNameTextBox")!;
+        var password = (PasswordBox)FindName("PasswordTextBox")!;
+
+        var connectionString = 
+            $"Server={databaseAddress.Text};" +
+            $"Database={databaseName.Text};" +
+            $"User ID={username.Text};" +
+            $"Password={password.Password}";
+        
+        var connection = new SqlConnection(connectionString);
+        try
+        {
+            connection.Open();
+            MessageBox.Show("Connection successful!");
+            DatabaseManager.Connection = connection;
+            Close();
+
+        }
+        catch (Exception exception)
+        {
+            MessageBox.Show($"Big Problem: {exception}");
+        }
+    }
+}
---
+++ lab/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+
+namespace lab
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectDB_OnClick(object sender, RoutedEventArgs e)
+        {
+            var connectionDialog = new ConnectionDialog();
+            connectionDialog.ShowDialog();
+        }
+
+        private void DisconnectDB_OnClick(object sender, RoutedEventArgs e)
+        {
+            DatabaseManager.Connection?.Close();
+            DatabaseManager.Connection = null;
+
+            MessageBox.Show("Disconnected from database");
+        }
+    }
+}
